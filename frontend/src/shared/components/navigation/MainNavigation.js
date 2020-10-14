ea97c5cedcf1db54fbc05241b0de2d84344c780b
Attila <59293104+sweeptic@users.noreply.github.com>
2020-10-14 11:46:41 +0200

    48. animating the sideDrawer

--- frontend/src/shared/components/navigation/MainNavigation.js
+++ frontend/src/shared/components/navigation/MainNavigation.js
@@ -1,31 +1,34 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-import MainHeader from './MainHeader';
-import NavLinks from './NavLinks';
-import SideDrawer from './SideDrawer';
-import './MainNavigation.css';
-import Backdrop from '../UIElements/Backdrop';
+import MainHeader from "./MainHeader";
+import NavLinks from "./NavLinks";
+import SideDrawer from "./SideDrawer";
+import "./MainNavigation.css";
+import Backdrop from "../UIElements/Backdrop";
 
-const MainNavigation = props => {
+const MainNavigation = (props) => {
   const [drawerIsOpen, setDrawerIsOpen] = useState(false);
 
-  const openDrawer = () => setDrawerIsOpen(true)
+  const openDrawerHandler = () => setDrawerIsOpen(true);
 
-  const closeDrawer = () => setDrawerIsOpen(false)
+  const closeDrawerHandler = () => setDrawerIsOpen(false);
 
   return (
     <React.Fragment>
-       {drawerIsOpen && <Backdrop onClick={closeDrawer} />}
-       {drawerIsOpen && (
-     <SideDrawer>
+      {drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />}
+
+      <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
         <nav className="main-navigation__drawer-nav">
           <NavLinks />
         </nav>
-      </SideDrawer> 
-      )}
+      </SideDrawer>
+
       <MainHeader>
-        <button className="main-navigation__menu-btn" onClick = {openDrawer}>
+        <button
+          className="main-navigation__menu-btn"
+          onClick={openDrawerHandler}
+        >
           <span />
           <span />
           <span />
