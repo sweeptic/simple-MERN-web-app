9609dc57d15d5393a1ee607e223be89bc4f6f879
Attila <59293104+sweeptic@users.noreply.github.com>
2020-10-15 13:14:07 +0200

    57. managing state in the input component

--- frontend/src/places/pages/NewPlace.js
+++ frontend/src/places/pages/NewPlace.js
@@ -5,8 +5,14 @@ import Input from './../../shared/components/Formelements/Input';
 const NewPlace = () => {
   return (
     <form className="place-form">
-      {/* <Input type="text" label="Title" validators={[]} onChange={} /> */}
-      <Input type="text" label="Title" element="input" />
+      <Input
+        type="text"
+        label="Title"
+        element="input"
+        validators={[]}
+        // onChange={}
+        errorText="Please enter a valid title."
+      />
     </form>
   );
 };
