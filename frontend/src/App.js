8c36638e8c3e9d0f22eaadb1570bf75d0bdd5ce8
Attila <59293104+sweeptic@users.noreply.github.com>
2020-10-16 10:05:48 +0200

    62. starting work on the 'update place' page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import Users from './users/pages/Users';
 import NewPlace from './places/pages/NewPlace';
 import UserPlaces from './places/pages/UserPlaces';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
+import UpdatePlace from './places/pages/UpdatePlace';
 
 const App = () => {
   return (
@@ -19,6 +20,7 @@ const App = () => {
           <Route path="/" exact component={Users} />
           <Route path="/:userId/places" exact component={UserPlaces} />
           <Route path="/Places/new" exact component={NewPlace} />
+          <Route path="/Places/:placeId" component={UpdatePlace} />
           <Redirect to="/" />
         </Switch>
       </main>
