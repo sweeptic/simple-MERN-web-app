3364db28b832727123f56fd61d7bd7fbfc8b5c50
Attila <59293104+sweeptic@users.noreply.github.com>
2020-10-16 14:01:24 +0200

    adding auth context for app-wide state management

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'; //default
+import React, { useCallback, useState } from 'react'; //default
 import {
   BrowserRouter as Router,
   Route,
@@ -11,22 +11,37 @@ import UserPlaces from './places/pages/UserPlaces';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 import UpdatePlace from './places/pages/UpdatePlace';
 import Auth from './users/pages/Auth';
+import { AuthContext } from './shared/context/auth-context';
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = useCallback(() => {
+    setIsLoggedIn(true);
+  }, []);
+
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+  }, []);
+
   return (
-    <Router>
-      <MainNavigation />
-      <main>
-        <Switch>
-          <Route path="/" exact component={Users} />
-          <Route path="/:userId/places" exact component={UserPlaces} />
-          <Route path="/places/new" exact component={NewPlace} />
-          <Route path="/places/:placeId" component={UpdatePlace} />
-          <Route path="/auth" component={Auth} />
-          <Redirect to="/" />
-        </Switch>
-      </main>
-    </Router>
+    <AuthContext.Provider
+      value={{ isLoggedIn: isLoggedIn, login: login, logout: logout }}
+    >
+      <Router>
+        <MainNavigation />
+        <main>
+          <Switch>
+            <Route path="/" exact component={Users} />
+            <Route path="/:userId/places" exact component={UserPlaces} />
+            <Route path="/places/new" exact component={NewPlace} />
+            <Route path="/places/:placeId" component={UpdatePlace} />
+            <Route path="/auth" component={Auth} />
+            <Redirect to="/" />
+          </Switch>
+        </main>
+      </Router>
+    </AuthContext.Provider>
   );
 };
 
