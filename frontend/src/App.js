662430820f82cafc42edd0d29657938d9c361b45
Attila <59293104+sweeptic@users.noreply.github.com>
2020-10-16 13:01:38 +0200

    69. adding an 'auth' page & loggin form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import NewPlace from './places/pages/NewPlace';
 import UserPlaces from './places/pages/UserPlaces';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 import UpdatePlace from './places/pages/UpdatePlace';
+import Auth from './users/pages/Auth';
 
 const App = () => {
   return (
@@ -19,8 +20,9 @@ const App = () => {
         <Switch>
           <Route path="/" exact component={Users} />
           <Route path="/:userId/places" exact component={UserPlaces} />
-          <Route path="/Places/new" exact component={NewPlace} />
-          <Route path="/Places/:placeId" component={UpdatePlace} />
+          <Route path="/places/new" exact component={NewPlace} />
+          <Route path="/places/:placeId" component={UpdatePlace} />
+          <Route path="/auth" component={Auth} />
           <Redirect to="/" />
         </Switch>
       </main>
