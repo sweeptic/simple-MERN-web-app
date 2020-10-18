1211188ac755719f103368e35cd5359268b01a08
Attila <59293104+sweeptic@users.noreply.github.com>
2020-10-18 15:51:17 +0200

    create backend backbone

---
+++ backend/app.js
@@ -0,0 +1,32 @@
+// request -> middleware -> next() middleware -> res.send() response
+
+// const http = require('http');
+
+const express = require('express');
+
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/add-product', (req, res, next) => {
+  console.log('This is my homepage');
+  res.send(
+    '<form action="/product" method="POST"> <input type="text" name="title"/><button type="submit">Submit</button></form>'
+  );
+});
+
+app.use('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+  res.send('<h1>Hello from express</h1>');
+});
+
+app.listen(5000);
+
+// const server = http.createServer(app);
+// server.listen(3020);
