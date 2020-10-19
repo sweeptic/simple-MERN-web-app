0e929bbfd04b74786e9456569c9f62c6da1dcfae
Attila <59293104+sweeptic@users.noreply.github.com>
2020-10-19 11:25:31 +0200

    91. handling errors

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,15 @@ const app = express();
 
 app.use('/api/places', placesRoutes);
 
+//error handling middleware function
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || 'An unknown error occured!' });
+});
+
 // app.use('/api/users', usersRoutes);
 
 app.listen(5000);
--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -19,18 +19,38 @@ const DUMMY_PLACES = [
 
 // places
 router.get('/:pid', (req, res, next) => {
+  console.log('/:pid');
   const placeId = req.params.pid; //{pid: 'p1'}
   const place = DUMMY_PLACES.find(p => {
     return p.id === placeId;
   });
+
+  if (!place) {
+    // throw error -> sync
+    // next -> use case of async error
+    const error = new Error('Could not find a place for the provided id');
+    error.code = 404;
+    //  throw error; //already cancel execution
+    return next(error);
+  }
+
   res.json({ place }); //place:place
 });
 
 router.get('/user/:uid', (req, res, next) => {
+  console.log('/user/:uid');
   const userId = req.params.uid; //{pid: 'p1'}
   const place = DUMMY_PLACES.find(p => {
     return p.creator === userId;
   });
+
+  if (!place) {
+    const error = new Error('Could not find a place for the provided user id');
+    error.code = 404;
+    //  return next(error);
+    throw error;
+  }
+
   res.json({ place }); //place:place
 });
 
