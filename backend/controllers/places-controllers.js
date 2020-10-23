5cef79fa7a2004ffa9570d607990a9113e9674c0
Attila <59293104+sweeptic@users.noreply.github.com>
2020-10-23 14:54:28 +0200

    127. updating places

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -53,7 +53,6 @@ const getPlaceById = async (req, res, next) => {
 const getPlacesByUserId = async (req, res, next) => {
   // console.log('/user/:uid');
   const userId = req.params.uid; //{pid: 'p1'}
-
   let places;
   try {
     places = await Place.find({ creator: userId });
@@ -64,14 +63,12 @@ const getPlacesByUserId = async (req, res, next) => {
     );
     return next(error);
   }
-
   // console.log(DUMMY_PLACES);
   if (!places || places.length === 0) {
     return next(
       new HttpError('Could not find a places for the provided user id', 404)
     );
   }
-
   res.json({ places: places.map(place => place.toObject({ getters: true })) }); //place:place
 };
 
@@ -82,7 +79,6 @@ const createPlace = async (req, res, next) => {
       new HttpError('Invalid inputs passed, please check your data', 422)
     );
   }
-
   const { title, description, address, creator } = req.body;
   let coordinates;
   try {
@@ -112,32 +108,43 @@ const createPlace = async (req, res, next) => {
   res.status(201).json({ place: createdPlace });
 };
 
-const updatePlace = (req, res, next) => {
+const updatePlace = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     throw new HttpError('Invalid inputs passed, please check your data', 422);
   }
-
   const { title, description } = req.body; //url + body mix
   const placeId = req.params.pid;
-
-  const updatedPlace = { ...DUMMY_PLACES.find(p => p.id === placeId) };
-  const placeIndex = DUMMY_PLACES.findIndex(p => p.id === placeId);
-  updatedPlace.title = title;
-  updatedPlace.description = description;
-
-  DUMMY_PLACES[placeIndex] = updatedPlace;
-
-  res.status(200).json({ place: updatedPlace });
+  let place;
+  try {
+    place = await Place.findById(placeId);
+  } catch (err) {
+    const error = new HttpError(
+      'Something went wrong  - in find, could not update a place',
+      500
+    );
+    return next(error);
+  }
+  place.title = title;
+  place.description = description;
+  // DUMMY_PLACES[placeIndex] = updatedPlace;
+  try {
+    await place.save();
+  } catch (err) {
+    const error = new HttpError(
+      'Something went wrong - in save, could not update a place ',
+      500
+    );
+    return next(error);
+  }
+  res.status(200).json({ place: place.toObject({ getters: true }) });
 };
 
 const deletePlace = (req, res, next) => {
   const placeId = req.params.pid;
-
   if (!DUMMY_PLACES.find(p => p.id !== placeId)) {
     throw new HttpError('Could not find a place for that id.', 404);
   }
-
   DUMMY_PLACES = DUMMY_PLACES.filter(p => p.id !== placeId);
   res.status(200).json({ message: 'Deleted place.' });
 };
