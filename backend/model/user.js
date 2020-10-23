dc0f0ff1401b5015b392e46c6a630c8a01ae98ed
Attila <59293104+sweeptic@users.noreply.github.com>
2020-10-23 15:12:26 +0200

    130. Creting the user model

---
+++ backend/model/user.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+const Schema = mongoose.Schema;
+
+userSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true, minlength: 6 },
+  image: { type: String, required: true },
+  places: { type: String, required: true }, //later will change
+});
+
+userSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model('User', userSchema);
