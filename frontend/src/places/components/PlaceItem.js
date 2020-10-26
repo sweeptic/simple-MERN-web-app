29c5958f613b6d9bf6d8f673c19314701082c4b7
Attila <59293104+sweeptic@users.noreply.github.com>
2020-10-26 14:10:14 +0100

    158. final adjusment

--- frontend/src/places/components/PlaceItem.js
+++ frontend/src/places/components/PlaceItem.js
@@ -95,11 +95,11 @@ const PlaceItem = props => {
               VIEW ON MAP
             </Button>
 
-            {auth.isLoggedIn && (
+            {auth.userId === props.creatorId && (
               <Button to={`/places/${props.id}`}>EDIT</Button>
             )}
 
-            {auth.isLoggedIn && (
+            {auth.userId === props.creatorId && (
               <Button danger onClick={showDeleteWarningHandler}>
                 DELETE
               </Button>
