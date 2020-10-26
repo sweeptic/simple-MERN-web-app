769109bc6db5fc72d65e748f12bf26a297eb6f92
Attila <59293104+sweeptic@users.noreply.github.com>
2020-10-26 13:02:03 +0100

    154. loading places by userid

--- frontend/src/places/components/PlaceList.js
+++ frontend/src/places/components/PlaceList.js
@@ -4,24 +4,24 @@ import Card from './../../shared/components/UIElements/Card';
 import PlaceItem from './PlaceItem';
 import Button from './../../shared/components/Formelements/Button';
 
-const PlaceList = (props) => {
+const PlaceList = props => {
   if (props.items.length === 0) {
     return (
-      <div className="place-list center">
+      <div className='place-list center'>
         <Card>
           <h2>No places found. Maybe create one?</h2>
-          <Button to="/places/new">Share Place</Button>
+          <Button to='/places/new'>Share Place</Button>
         </Card>
       </div>
     );
   }
   return (
-    <ul className="place-list">
-      {props.items.map((place) => (
+    <ul className='place-list'>
+      {props.items.map(place => (
         <PlaceItem
           key={place.id}
           id={place.id}
-          image={place.imageUrl}
+          image={place.image}
           title={place.title}
           description={place.description}
           address={place.address}
