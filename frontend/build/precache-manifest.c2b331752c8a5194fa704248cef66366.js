e6840254674cbbc45bb8c0aee86b8901cfa1a740
Attila <59293104+sweeptic@users.noreply.github.com>
2020-10-29 10:11:23 +0100

    197. adding code splitting to react

---
+++ frontend/build/precache-manifest.c2b331752c8a5194fa704248cef66366.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84edf387b76938ad9c97cf875c2bef4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "904456b2d8c9c502438d",
+    "url": "/static/css/main.ec027e31.chunk.css"
+  },
+  {
+    "revision": "86074432086a81d67f49",
+    "url": "/static/js/2.d213e243.chunk.js"
+  },
+  {
+    "revision": "904456b2d8c9c502438d",
+    "url": "/static/js/main.08ba066d.chunk.js"
+  },
+  {
+    "revision": "51810371bf2cf35f1e22",
+    "url": "/static/js/runtime-main.85498786.js"
+  }
+]);
